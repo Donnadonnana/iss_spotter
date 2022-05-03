01969382039b0c9c4e9e133ea0db5c12ab12de02
donnahuang <donnaphzj@gmail.com>
2022-05-03 09:53:01 -0400

    promised added

---
+++ index2.js
@@ -0,0 +1,16 @@
+// const { fetchMyIP, fetchCoordsByIP,fetchISSFlyOverTimes} = require('./iss_promise');
+
+// fetchMyIP()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then(body => console.log(body));
+
+const { nextISSTimesForMyLocation } = require('./iss_promise');
+
+ nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
